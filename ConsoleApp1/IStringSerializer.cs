a7824f46c5fb0f5244611db0964adf7756929474
MolodetskayaK <60287193+MolodetskayaK@users.noreply.github.com>
2026-01-17 01:05:10 +0700

    Delete ConsoleApp1 directory

--- ConsoleApp1/IStringSerializer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public interface IStringSerializer
-{
-    string Serialize(object obj);
-    object Deserialize(string data, Type type);
-    T Deserialize<T>(string data) where T : new();
-}
